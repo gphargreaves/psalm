c271ffc81d8826338dbfb2b98999a7047f783b06
Gregory Hargreaves <gphargreaves@googlemail.com>
2022-10-07 01:45:39 +0100

    Add check for const with reserved word class

--- tests/ClassTest.php
+++ tests/ClassTest.php
@@ -925,6 +925,19 @@ class ClassTest extends TestCase
                 ',
                 'error_message' => 'InvalidTraversableImplementation',
             ],
+            'cannotNameClassConstantClass' => [
+                '<?php
+
+                class Foo
+                {
+                    /** @var class-string<Bar> */
+                    protected const CLASS = Bar::class;
+                }
+                
+                class Bar {}
+                ',
+                'error_message' => 'ReservedWord',
+            ]
         ];
     }
 }
